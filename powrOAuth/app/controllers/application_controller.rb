8fa3698bc9353b6166496ce9ae0f41af684c0ebb
rustamata <rustamkin@gmail.com>
2019-06-13 01:04:28 +0600

    Following functions moved from application_controller to login_controller: save_token_info_to_session, clean_token_info_inside_session, user_has_privilege?, get_session_token New functions in login_controller: get_client_id, get_client_secret, get_app_name (return environment variables values) New functions get_session_token and get_app_name in pages_controller New flash messages when user updates index.html route "login#github" renamed to "login#oauth" + some refactoring

--- powrOAuth/app/controllers/application_controller.rb
+++ powrOAuth/app/controllers/application_controller.rb
@@ -1,39 +1,16 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate, except: [:callback, :index, :show, :github]
-
-  CLIENT_ID = ENV['GH_BASIC_CLIENT_ID']
-  CLIENT_SECRET = ENV['GH_BASIC_SECRET_ID']
-  APP_NAME = ENV['GH_BASIC_APP_NAME']
+  before_action :authenticate, except: [:callback, :show, :oauth]
 
   use Rack::Session::Pool, :cookie_only => false
 
-  protected def save_token_info_to_session(response)
-    session[:access_token] = response['access_token']
-    session[:scope] = response['scope']
-  end
-
-  protected def clean_token_info_inside_session
-    session[:access_token] = nil
-    session[:scope] = nil
-  end
-
-  protected def user_has_privilege?(privilege)
-    scope = session[:scope]
-    return scope.include? privilege
-  end
-
-  protected def get_session_token
-    return session[:access_token]
-  end
-
-  protected def authenticated?
+  private def authenticated?
     session[:access_token]
   end
 
   private def authenticate
     if !authenticated?
-      redirect_to "/login"
+      redirect_to login_path
     end
   end
-  
+
 end
--- powrOAuth/app/controllers/login_controller.rb
+++ powrOAuth/app/controllers/login_controller.rb
@@ -1,57 +1,88 @@
 class LoginController < ApplicationController
 
-  def show
-    #Show login form to unauthorized users only
+  def show #show login page
+    #Do not show login form to authenticated users
     if authenticated?
-      redirect_to "/pages/edit"
+      redirect_to pages_edit_path
     end
   end
 
-  def github
-    redirect_to "https://github.com/login/oauth/authorize?state=#{SecureRandom.uuid}&scope=read:user,user:email,repo&client_id=#{ENV['GH_BASIC_CLIENT_ID']}"
+  def oauth #go to GitHub for authorization
+    redirect_to "https://github.com/login/oauth/authorize?state=#{get_guid}&scope=read:user,user:email,repo&client_id=#{get_client_id}"
   end
 
-  def callback
-    save_token_info_to_session(get_github_token_info(params[:code], params[:state]))
-    save_user_info_to_db
-    if authenticated?
-      redirect_to "/pages/edit"
-    else
-      redirect_to "/login"
-    end
+  def callback #receive callback from GitHub
+    save_session_token(get_github_token(params[:code], params[:state]))
+    save_user_to_db
+    redirect_to pages_edit_path
   end
 
-  def logout
-    clean_token_info_inside_session
-    redirect_to "/login"
+  def logout #logout from app
+    destroy_session_token
+    redirect_to login_path
   end
 
-  private def get_github_token_info(access_code, auth_state)
+
+  private def get_github_token(access_code, auth_state) #get token from GitHub
     begin
       return HTTParty.post('https://github.com/login/oauth/access_token',
-                           body: {client_id: CLIENT_ID, client_secret: CLIENT_SECRET, code: access_code, state: auth_state},
+                           body: {client_id: get_client_id, client_secret: get_client_secret, code: access_code, state: auth_state},
                            headers: {Accept: "application/json"})
     rescue
       raise "Unable to get token from GitHub, #{$!}"
     end
   end
 
-
-  private def get_github_user_info
+  private def get_github_user #get user info from GitHub
     begin
       return HTTParty.get('https://api.github.com/user',
-                          headers: {"User-Agent": "powerfulOAuth", Authorization: "token #{get_session_token}", Accept: "application/json"})
+                          headers: {"User-Agent": get_app_name, Authorization: "token #{get_session_token}", Accept: "application/json"})
     rescue
       raise "Unable to get user info from GitHub, #{$!}"
     end
   end
 
-  private def save_user_info_to_db
+  private def save_user_to_db #save GitHub user info to our DB
     if user_has_privilege?('read:user')
-      User.save_user(get_github_user_info)
+      User.save_user(get_github_user)
     else
       raise "Insufficient privileges, can't save user to DB"
     end
+  end
+
+  private def get_session_token #get GitHub token from session
+    session[:access_token]
+  end
+
+  private def save_session_token(response) #save GitHub token and scope to session
+    session[:access_token] = response['access_token']
+    session[:scope] = response['scope']
+  end
+
+  private def destroy_session_token #delete GitHub token and scope from session
+    session.delete(:access_token)
+    session.delete(:scope)
+  end
+
+  private def user_has_privilege?(privilege) #check if user has privilege (true/false)
+    scope = session[:scope]
+    scope.include? privilege
+  end
+
+  private def get_client_id #get application's client_id from environment variable
+    ENV['GH_BASIC_CLIENT_ID']
+  end
+
+  private def get_client_secret #get application's client_secret from environment variable
+    ENV['GH_BASIC_SECRET_ID']
+  end
+
+  private def get_app_name #get application's name from environment variable
+    ENV['GH_BASIC_APP_NAME']
+  end
+
+  private def get_guid
+    SecureRandom.uuid
   end
 
 end
